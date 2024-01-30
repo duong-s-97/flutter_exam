73b04989271ff8e8094f3fae99e4644762a936dd
duong-s-97 <duongdev97@gmail.com>
2024-01-30 21:38:13 +0700

    d

---
+++ lib/tabbar.dart
@@ -0,0 +1,48 @@
+import 'package:flutter/material.dart';
+
+class TabBarExample extends StatefulWidget {
+  const TabBarExample({super.key});
+
+  @override
+  State<TabBarExample> createState() => _TabBarExampleState();
+}
+
+class _TabBarExampleState extends State<TabBarExample>
+    with TickerProviderStateMixin {
+  @override
+  Widget build(BuildContext context) {
+    TabController _tabController = TabController(length: 2, vsync: this);
+    return Container(
+      color: Colors.white,
+      child: SafeArea(
+        child: Scaffold(
+          body: Column(
+            children: [
+              Container(
+                width: double.infinity,
+                height: 100,
+                color: Colors.red,
+              ),
+              Container(
+                child: Align(
+                  alignment: Alignment.centerLeft,
+                  child: TabBar(
+                    controller: _tabController,
+                    tabs: [
+                      Tab(
+                        text: "Like",
+                      ),
+                      Tab(
+                        text: "Search",
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
